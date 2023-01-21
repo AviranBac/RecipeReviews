eef8031a952d3bf5b4f87735d5d29f1617646ec1
Aviran Bachar <aviranbac1@gmail.com>
2023-01-21 14:31:36 +0200

    fix(Firebase): restore isSignedIn method

--- app/src/main/java/com/example/recipereviews/models/firebase/AuthFirebase.java
+++ app/src/main/java/com/example/recipereviews/models/firebase/AuthFirebase.java
@@ -43,7 +43,7 @@ public class AuthFirebase {
     }
 
     public boolean isSignedIn() {
-        return false;
+        return firebaseAuth.getCurrentUser() != null;
     }
 
     public String getCurrentUserUid() {
