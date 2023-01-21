f8dd48051e8e13a49c1577c241bbe13c11475aad
Aviran Bachar <aviranbac1@gmail.com>
2023-01-21 02:36:57 +0200

    feat(Navigation and Styling): add action bar and "back" button to it, as well as intense struggle with style

--- app/src/main/java/com/example/recipereviews/activities/GuestActivity.java
+++ app/src/main/java/com/example/recipereviews/activities/GuestActivity.java
@@ -1,16 +1,36 @@
 package com.example.recipereviews.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.recipereviews.R;
 
 public class GuestActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.guests_nav_host);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            navController.popBackStack();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
